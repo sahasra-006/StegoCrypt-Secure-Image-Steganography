83365a8b3547b1f36547cac78e74e549db3895e8
A. Sahasra <sahasra.alugubelli@gmail.com>
2026-04-26 13:10:13 +0530

    Update settings.py

--- steganography_project/settings.py
+++ steganography_project/settings.py
@@ -95,9 +95,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pixelthorn1@gmail.com'       # ← your Gmail address
-EMAIL_HOST_PASSWORD = 'oxvt ibvb lhgm peof'    # ← Gmail App Password (NOT your login password)
-DEFAULT_FROM_EMAIL = 'pixelthorn1@gmail.com'
+EMAIL_HOST_USER = 'your@gmail.com'       # ← your Gmail address
+EMAIL_HOST_PASSWORD = 'your_password'    # ← Gmail App Password (NOT your login password)
+DEFAULT_FROM_EMAIL = 'yourgmail@gmail.com'
 
 
 
